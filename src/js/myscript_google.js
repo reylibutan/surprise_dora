a295222702bb086da1cfdb132eadb645cc20ab73
reylibutan <reylibutan@gmail.com>
2014-11-24 06:31:35 +0800

    Replace Google logo

--- src/js/myscript_google.js
+++ src/js/myscript_google.js
@@ -1,9 +1,15 @@
 var LANG_ENGLISH = "English";
 var GOOGLE_DOT_COM = "http://google.com.ph";
+var LUCKY_BTN_ID = "gbqfbb";
 
 $(document).ready(function() {
+	//must be Google site (not new tab)
+	//must be in English (to show "I'm feeling Lucky")
 	redirectToActualGoogle();
 	setGoogleInEnglish();
+
+	//replace Google logo with 4th Anniversary Picture
+	replaceGoogleLogo();
 });
 
 function redirectToActualGoogle() {
@@ -39,4 +45,19 @@ function isGoogleInEnglish() {
 	});
 
 	return !(englishGoogleUrl.length);
+}
+
+function replaceGoogleLogo() {
+	var logo_div = $("#hplogo");
+	var anniv_logo = "http://fc03.deviantart.net/fs70/f/2012/145/1/4/happy_4th_anniversary__by_knotberry-d5122gn.jpg";
+	var img_el = logo_div.find("a:first img");
+
+	//hide share button
+	$("div._P7b").hide();
+
+	img_el.fadeOut(750, function() {
+		img_el.addClass("custom_google_logo");
+		img_el.prop("src", anniv_logo);
+		img_el.fadeIn(750);
+	});
 }
