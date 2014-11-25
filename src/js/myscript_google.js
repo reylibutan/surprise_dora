f3ca23dbcaba42fb26dd88b7f207cf81b5bb87c9
reylibutan <reylibutan@gmail.com>
2014-11-25 22:53:24 +0800

    Merged from workstation

--- src/js/myscript_google.js
+++ src/js/myscript_google.js
@@ -1,5 +1,5 @@
 var LANG_ENGLISH = "English";
-var GOOGLE_DOT_COM = "http://google.com.ph";
+var GOOGLE_DOT_COM = "https://google.com";
 var LUCKY_BTN_ID = "gbqfbb";
 
 $(document).ready(function() {
@@ -10,6 +10,9 @@ $(document).ready(function() {
 
 	//replace Google logo with 4th Anniversary Picture
 	replaceGoogleLogo();
+
+	//"I'm Feeling Lhuby"
+	initImFeelingLhubyButton();
 });
 
 function redirectToActualGoogle() {
@@ -40,24 +43,44 @@ function setGoogleInEnglish() {
 }
 
 function isGoogleInEnglish() {
-	var englishGoogleUrl = $("div#_eEe a").filter(function() {
+	var $englishGoogleUrl = $("div#_eEe a").filter(function() {
 		return $(this).html() === LANG_ENGLISH;
 	});
 
-	return !(englishGoogleUrl.length);
+	return !($englishGoogleUrl.length);
 }
 
 function replaceGoogleLogo() {
-	var logo_div = $("#hplogo");
-	var anniv_logo = "http://fc03.deviantart.net/fs70/f/2012/145/1/4/happy_4th_anniversary__by_knotberry-d5122gn.jpg";
-	var img_el = logo_div.find("a:first img");
+	var $logo_div = $("#hplogo");
+	var $img_el = $logo_div.find("a:first img");
+	var anniv_logo = "http://fc03.deviantart.net/fs70/f/2012/145/1/4/happy_4th_anniversary__by_knotberry-d5122gn.jpg";	
 
 	//hide share button
 	$("div._P7b").hide();
 
-	img_el.fadeOut(750, function() {
-		img_el.addClass("custom_google_logo");
-		img_el.prop("src", anniv_logo);
-		img_el.fadeIn(750);
+	$img_el.fadeOut(750, function() {
+		$img_el.addClass("custom_google_logo");
+		$img_el.prop("src", anniv_logo);
+		$img_el.fadeIn(750);
 	});
+}
+
+function initImFeelingLhubyButton() {
+	var $btn_div = $("#gbqfbwa");
+	var lhuby_btn = document.createElement("input");
+	var $lhuby_btn = $(lhuby_btn);
+
+	lhuby_btn.type = "button";
+	lhuby_btn.value = "I'm feeling Lhuby!";
+	lhuby_btn.className = "gbqfba glowing-pink";
+
+	$lhuby_btn.css("background", "#FFFFFF");
+	$lhuby_btn.find("span").html("Im feeling Lhuby!");
+	$lhuby_btn.addClass('glowing-pink');
+
+	$lhuby_btn.click(function(e) {
+		alert();
+	});
+    
+    $btn_div.append(lhuby_btn);
 }
